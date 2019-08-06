b53b7c29e544c941779c37deaf36cd955f900105
Russell Villanueva Work Computer <villanuevarussell@yahoo.com>
2019-08-05 20:37:54 -0700

    Modified NewPRDeploy.py and OldPRdeploy.py Final Commit

--- NewPRDeploy.py
+++ NewPRDeploy.py
@@ -4,19 +4,32 @@ from ssh import ssh
 import time
 
 def deploy(ip, username, password, source, destination):
-	print(f"Connecting to {ip}")
-	connection = ssh(ip, username, password)#connects to remote desktop
-	connection.openShell()#makes connection to remote 
-	connection.sendShell(f"rm -rf ~/Desktop/firmware")#removes all files and directories currently in folder
-	print(f"Moving files from {source} to {destination}")
-	connection.sendShell(f"mv {source} {destination}")#moves all contents from source directory to destination directory
-	time.sleep(1)#gives time for command to execute
+	f = open("Deploy.log", "a+")
+	f.write(f"NewPRDeploy.py")
 
-	tempfolder = source[:-1]#removes last character from source variable, removes the * so the directory can be deleted
-	connection.sendShell(f"rm -rf {tempfolder}")#removes temp folder from Desktop
-	time.sleep(1)
-	connection.closeConnection()#closes Connection between host and remote desktop
+	try:
+		print(f"Connecting to {ip}")
+		f.write(f"Connecting to {ip}")
 
+		connection = ssh(ip, username, password)#connects to remote desktop
+		connection.openShell()#makes connection to remote 
+		connection.sendShell(f"rm -rf ~/Desktop/firmware")#removes all files and directories currently in folder
+		print(f"Moving files from {source} to {destination}")
+		connection.sendShell(f"mv {source} {destination}")#moves all contents from source directory to destination directory
+		time.sleep(1)#gives time for command to execute
+
+		tempfolder = source[:-1]#removes last character from source variable, removes the * so the directory can be deleted
+		connection.sendShell(f"rm -rf {tempfolder}")#removes temp folder from Desktop
+		time.sleep(1)
+		connection.closeConnection()#closes Connection between host and remote desktop
+		print(f"************{ip} PASS********************\n")
+		f.write(f"************{ip} PASS********************\n")#Writes a pass on logs
+	except:
+		print(f"Could not deploy, was not able to connect to {ip}")
+		f.write(f"************{ip} FAIL********************\n")#writes to log
+		f.write(f"IPSW Files were not Deployed to {ip}")#writes to log
+
+	f.close()
 
 if __name__ == "__main__":
 	#These are only parameters that need to be changed
--- OldPRDeploy.py
+++ OldPRDeploy.py
@@ -4,19 +4,28 @@ from ssh import ssh
 import time
 
 def deploy(ip, username, password, source, destination):
-	print(f"Connecting to {ip}")
-	connection = ssh(ip, username, password)#connects to remote desktop
-	connection.openShell()#makes connection to remote 
-	connection.sendShell(f"rm -rf {destination}/*")#removes all files and directories currently in folder
-	print(f"Moving files from {source} to {destination}")
-	connection.sendShell(f"mv {source} {destination}")#moves all contents from source directory to destination directory
-	time.sleep(1)#gives time for command to execute
+	f = open("Deploy.log", "a+")
+	f.write(f"OldPRDeploy.py")
+	
+	try:
+		print(f"Connecting to {ip}")
+		f.write(f"Connecting to {ip}")
 
-	tempfolder = source[:-1]#removes last character from source variable, removes the * so the directory can be deleted
-	time.sleep(1)
-	connection.sendShell(f"rm -rf {tempfolder}")#removes temp folder from Desktop
-	connection.closeConnection()#closes Connection between host and remote desktop
+		connection = ssh(ip, username, password)#connects to remote desktop
+		connection.openShell()#makes connection to remote 
+		connection.sendShell(f"rm -rf {destination}/*")#removes all files and directories currently in folder
+		print(f"Moving files from {source} to {destination}")
+		connection.sendShell(f"mv {source} {destination}")#moves all contents from source directory to destination directory
+		time.sleep(1)#gives time for command to execute
 
+		tempfolder = source[:-1]#removes last character from source variable, removes the * so the directory can be deleted
+		time.sleep(1)
+		connection.sendShell(f"rm -rf {tempfolder}")#removes temp folder from Desktop
+		connection.closeConnection()#closes Connection between host and remote desktop
+	except:
+			print(f"Could not deploy, was not able to connect to {ip}")
+		f.write(f"************{ip} FAIL********************\n")#writes to log
+		f.write(f"IPSW Files were not Deployed to {ip}")#writes to log
 
 if __name__ == "__main__":
 	#These are only parameters that need to be changed
